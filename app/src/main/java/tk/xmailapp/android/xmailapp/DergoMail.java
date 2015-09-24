72e3c0bcf5dc4942be9e9d6ea8a0587a6959a6c9
bledar <bledi-1@live.com>
2015-09-24 21:41:08 +0200

    Komiti i pare dizajni i perfunduar

---
+++ app/src/main/java/tk/xmailapp/android/xmailapp/DergoMail.java
@@ -0,0 +1,13 @@
+package tk.xmailapp.android.xmailapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class DergoMail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dergo_mail);
+    }
+
+}
