055b0812b3901de9f33e44e4138bcae05961c858
bledar <bledi-1@live.com>
2015-09-24 22:15:04 +0200

    rregullim te vogla

--- app/src/main/java/tk/xmailapp/android/xmailapp/DergoMail.java
+++ app/src/main/java/tk/xmailapp/android/xmailapp/DergoMail.java
@@ -2,12 +2,15 @@ package tk.xmailapp.android.xmailapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 public class DergoMail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_dergo_mail);
     }
+
 
 }
