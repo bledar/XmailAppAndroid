16aa42a194bb1e4d816865a92f353cb99a080b75
bledar <bledi-1@live.com>
2015-09-26 23:07:53 +0200

    Dergon mesazhe ne server dhe merr pergjie nga serveri duke analizuar pergjigjen dhe gabimet

--- app/src/main/java/tk/xmailapp/android/xmailapp/DergoMail.java
+++ app/src/main/java/tk/xmailapp/android/xmailapp/DergoMail.java
@@ -1,19 +1,21 @@
 package tk.xmailapp.android.xmailapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -22,6 +24,16 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+/*
+
+Klasa DergoMail eshte baza e aplikacionit tone pasi ketu behen te gjitha inicializimet fillestare
+dhe merren te gjitha te dhenat e nevojshme dhe pregatiten per dergimin e emailt
+Gjithashtu ketu behet edhe menagjimi i te gjitha gabimeve te mundshme
+Ajo trashegohet nga klasa baze e Androidit qe menagjon Aktivitet
+
+*/
 
 public class DergoMail extends Activity {
 
@@ -29,10 +41,12 @@ public class DergoMail extends Activity {
     EditText fusha_dergusit ;
     EditText fusha_subjektit;
     EditText fusha_mesazhit;
-    //ProgressBar progBar;
     Button btn_dego;
-
+    ProgressDialog progress;
+    String njoftimi  = "Bosh";//Variabli qe ruan njoftimin qe do shfaqet per perdoruesin.
+    boolean sukses=false; //Behet true ne momentin qe te dhenat jane derguar me sukses ne server.
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dergo_mail);
@@ -40,32 +54,26 @@ public class DergoMail extends Activity {
         fusha_dergusit   = (EditText) findViewById(R.id.fusha_derguesi);
         fusha_subjektit  = (EditText) findViewById(R.id.fusha_subjektit);
         fusha_mesazhit   = (EditText) findViewById(R.id.fusha_mesazhit);
-        //progBar          = (ProgressBar) findViewById(R.id.progBar);
         btn_dego         = (Button) findViewById(R.id.btn_dergo);
-        //progBar.setVisibility(View.GONE);
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_dergo_mail, menu);
+        fusha_marresit.setText("bledi-1@live.com");
+        fusha_dergusit.setText("bledihaxhia@gmail.com");
+        fusha_subjektit.setText("dergoje");
+        fusha_mesazhit.setText("Nje mesazh i derguar nga android.");
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            // action with ID action_refresh was selected
+
             case R.id.btn_dergo:
                 dergoMail();
-                break;
-            // action with ID action_settings was selected
-            case R.id.action_settings:
-                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
-                        .show();
-                break;
-            default:
                 break;
         }
 
@@ -78,13 +86,12 @@ public class DergoMail extends Activity {
         String mesazhi=fusha_mesazhit.getText().toString();
 
         if(fusha_dergusit.length()<2&&fusha_marresit.length()<2&&fusha_subjektit.length()<2&&fusha_mesazhit.length()<2){
-
-            Toast.makeText(getApplicationContext(), "Ju lutem plotesoni formen!", Toast.LENGTH_LONG).show();
+            njoftimi="Ju lutem plotesoni formen!";
+            Toast.makeText(getApplicationContext(),njoftimi , Toast.LENGTH_LONG).show();
 
         }else{
-            //progBar.setVisibility(View.VISIBLE);
+            progress = ProgressDialog.show(this, "XmailApp","Emaili po dergohet ne server!", true);
             new DergoNeServer().execute(marresi, derguesi, subjekti, mesazhi);
-            //Toast.makeText(getApplicationContext(), "Emaili po dergohet", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -103,7 +110,7 @@ public class DergoMail extends Activity {
         public void postData(String marresi,String derguesi,String subjekti,String mesazhi) {
             // Create a new HttpClient and Post Header
             HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://bledarhaxhia.com/xmailapp.tk/proqesmail.php");
+            HttpPost httppost = new HttpPost("http://xmailapp.tk/proqesmail.php");
 
             try {
                 // Add your data
@@ -116,23 +123,58 @@ public class DergoMail extends Activity {
 
                 // Execute HTTP Post Request
                 HttpResponse response = httpclient.execute(httppost);
+                int responseCode = response.getStatusLine().getStatusCode();
+                switch(responseCode) {
+                    case 200:
+                        HttpEntity entity = response.getEntity();
+                        if(entity != null) {
+
+                            String responseBody = EntityUtils.toString(entity);
+                            Log.v("Gabim", "Pergjigjia e serverit: "+responseBody);
+                            njoftimi= ""+responseBody;
+                            sukses=true;
+                        }
+                        break;
+                }
+
+                Log.v("Gabim", "Kodi i serverit: "+responseCode);
 
             } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
+
+                Log.v("Gabim", "Gabin ne protokoll");
+                sukses=false;
+
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+                Log.v("Gabim", "Gabin ne komunikim");
+                sukses=false;
             }
         }
 
         protected void onPostExecute(Double result){
-            Toast.makeText(getApplicationContext(), "Emaili u DERGUA", Toast.LENGTH_LONG).show();
-            //progBar.setVisibility(View.GONE);
-        }
+            fusha_marresit.setText("");
+            fusha_dergusit.setText("");
+            fusha_subjektit.setText("");
+            fusha_mesazhit.setText("");
+            progress.dismiss();
+            if(sukses){
+                Toast.makeText(getApplicationContext(), njoftimi, Toast.LENGTH_LONG).show();
+            }else {
+                Njoftimi();
+            }
 
-        protected void onProgressUpdate(Integer... progress){
-           // progBar.setProgress(progress[0]);
+
         }
     }
+
+    public void Njoftimi(){
+        AlertDialog.Builder Alerti = new AlertDialog.Builder(DergoMail.this);
+        Alerti.setCancelable(true);
+        Alerti.setMessage("Problem ne dergimin e te dhenave ne server.\n Sigurohuni qe keni nje lidhje me internetin!");
+        Alerti.setTitle("GABIM KOMUNIKIMI");
+        AlertDialog dialog = Alerti.create();
+        dialog.show();
+    }
+
 }
 
 
